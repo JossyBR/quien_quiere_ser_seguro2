f744808c845914cbdcf1a75a89b34a796e12da48
Jossy Benitez <jossybr94@gmail.com>
2024-04-26 16:29:12 -0500

    Instalacion de dependencias trabajando en algunos componentes para el front

---
+++ client/src/Components/FormularioPostEdit.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+
+const FormularioPostEdit = () => {
+  // const [form, setForm] = useState({
+  //     pregunta: "",
+  //     respuesta1: "",
+  //     respuesta2: "",
+  //     respuesta3: "",
+  //     respuesta4: "",
+  //     respuesta_correcta: "",
+  // });
+
+  // const handleChange = (e) => {
+  //     const { name, value } = e.target;
+  //     setForm({ ...form, [name]: value });
+  // };
+
+  // const handleSubmit = (e) => {
+  //     e.preventDefault();
+  //     Inertia.post("/pregunta", form);
+  // };
+
+  return (
+    <div>
+      <form>
+        <div>
+          {/* <div>
+                        <Link href="/admin/preguntas">Regresar</Link>
+                    </div>
+                    <label htmlFor="pregunta">Escribe una pregunta</label> */}
+          <input
+            type="text"
+            id="pregunta"
+            name="pregunta"
+            // value={form.pregunta}
+            // onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="respuesta1">Respuesta 1</label>
+          <input
+            type="text"
+            id="respuesta1"
+            name="respuesta1"
+            // value={form.respuesta1}
+            // onChange={handleChange}
+            required
+          />
+          /{" "}
+        </div>
+        <div>
+          <label htmlFor="respuesta2">Respuesta 2</label>
+          <input
+            type="text"
+            id="respuesta2"
+            name="respuesta2"
+            // value={form.respuesta2}
+            // onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="respuesta3">Respuesta 3</label>
+          <input
+            type="text"
+            id="respuesta3"
+            name="respuesta3"
+            // value={form.respuesta3}
+            // onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="respuesta4">Respuesta 4</label>
+          <input
+            type="text"
+            id="respuesta4"
+            name="respuesta4"
+            // value={form.respuesta4}
+            // onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="respuesta_correcta">
+            Escoja la respuesta correcta
+          </label>
+          <select
+            id="respuesta_correcta"
+            name="respuesta_correcta"
+            // value={form.respuesta_correcta}
+            // onChange={handleChange}
+            required
+          >
+            {/* Asegúrate de que las opciones estén disponibles solo si la respuesta correspondiente no está vacía */}
+            {/* <option>Seleccione</option>
+                        {form.respuesta1 && (
+                            <option value={form.respuesta1}>
+                                {form.respuesta1}
+                            </option>
+                        )}
+                        {form.respuesta2 && (
+                            <option value={form.respuesta2}>
+                                {form.respuesta2}
+                            </option>
+                        )}
+                        {form.respuesta3 && (
+                            <option value={form.respuesta3}>
+                                {form.respuesta3}
+                            </option>
+                        )}
+                        {form.respuesta4 && (
+                            <option value={form.respuesta4}>
+                                {form.respuesta4}
+                            </option>
+                        )}*/}
+          </select>
+        </div>
+
+        <button type="submit">Crear pregunta</button>
+      </form>
+    </div>
+  );
+};
+
+export default FormularioPostEdit;
---
+++ client/src/pages/Admin/AdminPreguntas.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Inertia } from "@inertiajs/inertia";
+import { Link } from "@inertiajs/inertia-react";
+
+const AdminPreguntas = ({ preguntas }) => {
+  // const eliminarPregunta = (id) => {
+  //     // Código para eliminar pregunta
+  //     Inertia.delete(`/admin/preguntas/${id}`);
+  // };
+
+  return (
+    <div>
+      <h2>Administrar Preguntas</h2>
+
+      <div>
+        <Link href="/juego"> Regresar al juego</Link>
+      </div>
+      <div>
+        <Link href="">Crear Preguntas</Link>
+      </div>
+
+      {preguntas.map((pregunta) => (
+        <div key={pregunta.id}>
+          {pregunta.pregunta}
+          <button
+          // onClick={() =>
+          //     Inertia.visit(
+          //         `/admin/preguntas/editar/${pregunta.id}`
+          //     )
+          // }
+          >
+            Editar
+          </button>
+          <button
+          // onClick={() =>
+          //     Inertia.visit(
+          //         `/admin/preguntas/delete/${pregunta.id}`,
+          //         { method: "get" }
+          //     )
+          // }
+          >
+            Eliminar
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AdminPreguntas;
